67ddfe5807a057606ee8b89b9a87f01b45831ad8
omarAbdullahMoharam <omarmoharam790@gmail.com>
2025-12-08 23:04:16 +0200

    Refactor Account class to improve validation methods and enhance readability

--- src/com/omar/bank/model/Account.java
+++ src/com/omar/bank/model/Account.java
@@ -4,21 +4,10 @@ import com.omar.bank.exception.InsufficientAmountException;
 import com.omar.bank.exception.InvalidAccountException;
 import com.omar.bank.exception.InvalidAmountException;
 
-import static com.omar.bank.util.NumberFormatter.customFormatter;
-
 abstract public class Account {
     private static final int ACCOUNT_NUMBER_LENGTH = 16;
     protected static final double WITHDRAW_FEE_PERCENT =0.01 ;
-
-
-    //    validate: only numbers + length 16 + not changeable (Bank code + Branch code + Serial)
-    private final String accountNumber;
-    protected double balance;
-//
-    private Customer owner;
-//    Composition Relationship & validate accountNumber
-    public Account(String accountNumber, Customer owner)  {
-
+    private void validateAccountNumber(String accountNumber) throws InvalidAccountException {
         if (accountNumber == null) {
             throw new InvalidAccountException("Account number cannot be null");
         }
@@ -32,10 +21,36 @@ abstract public class Account {
         if (!accountNumber.matches("\\d{" + ACCOUNT_NUMBER_LENGTH + "}")){
             throw new InvalidAccountException("Account number must contain digits only");
         }
+    }
+    private void validateOwner(Customer owner) throws InvalidAccountException {
         if (owner == null) {
             throw new InvalidAccountException("Owner cannot be null");
         }
+
+    }
+
+    //    validate: only numbers + length 16 + not changeable (Bank code + Branch code + Serial)
+    private final String accountNumber;
+    protected double balance;
+    private Customer owner;
+
+
+//    Composition Relationship & validate accountNumber
+    public Account(String accountNumber, Customer owner)  {
+//      validate AccountNumber
+        try {
+            validateAccountNumber(accountNumber);
+        } catch (InvalidAccountException e) {
+            System.out.println(e.getMessage());
+        }
+//        validate owner
+        try {
+            validateOwner(owner);
+        } catch (InvalidAccountException e) {
+            System.out.println(e.getMessage());
+        }
         this.accountNumber = accountNumber;
+
         this.owner = owner;
         this.balance = 0;
     }
@@ -69,7 +84,7 @@ abstract public class Account {
 //        System.out.println();
 
     }
-    public abstract  double withdraw(double amount) throws InvalidAmountException, InsufficientAmountException;
+    public abstract double withdraw(double amount) throws InvalidAmountException, InsufficientAmountException;
 
 //    polymorphism
     @Override
